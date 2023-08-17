a79ad0b023b51732de2fb076b29ec0ef85a43442
daceyforward <dacey.loving@gmail.com>
2023-08-16 21:45:54 -0400

    my dev skills lab - part 2 complete

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Awesome Developer Skills' });
+  res.render('index', { title: 'Awesome Developer Skills'});
 });
-
-// app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = router;
--- routes/skills.js
+++ routes/skills.js
@@ -6,8 +6,17 @@ const skillsCtrl = require('../controllers/skills')
 
 // GET/ skills
 router.get('/', skillsCtrl.index)
+
+// GET /skills/new (this needs to be defined before show route)
+router.get('/new', skillsCtrl.new);
+
 //GET /skills/:id
 router.get('/:id', skillsCtrl.show)
 
+//POST /skills
+router.post('/', skillsCtrl.create);  
+
+// DELETE /skills/:id
+router.delete('/:id', skillsCtrl.delete);
 
 module.exports = router;
